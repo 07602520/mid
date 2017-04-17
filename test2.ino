3867d06345c28edf30e1bd725e54c8f717382663
07602520 <raymon3838@gmail.com>
2017-04-17 18:53:10 +0800

    Add files via upload

---
+++ test2.ino
@@ -0,0 +1,35 @@
+#include "SevSeg.h"
+SevSeg sevseg;
+int i=0;
+void setup() {
+  Serial.begin(9600);
+  Serial.println("33 04052520 04050760");
+   byte numDigits = 4;
+  byte digitPins[] = {5,4, 3, 2};
+  byte segmentPins[] = {6, 7, 8, 9, 10, 11, 12, 13};
+  sevseg.begin(COMMON_CATHODE, numDigits, digitPins,segmentPins);
+  
+}
+void loop() {
+  int i;
+  if(Serial.available())
+  {
+      i=Serial.parseInt();
+      if(i<=9999 && i>0)
+       {
+          Serial.println(i);
+          sevseg.setNumber(i);
+          sevseg.refreshDisplay();
+        }
+      else
+        {
+          Serial.println("range error");
+        }
+  }
+    else
+      {
+  sevseg.refreshDisplay();
+  
+  }
+}
+
